6a3c373fae30443dd9e593dcebe9e2e4866154e2
Sriram <sriram06sept@gmail.com>
2015-02-01 23:40:17 +0400

    Throw exception refactored; Moved to separate method.

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculatorKata
@@ -16,14 +17,19 @@ namespace StringCalculatorKata
                 .Select(item => item.TryParseOrDefault())
                 .ToList();
 
+            ThrowIfAnyNegative(numbersList);
+
+            return numbersList.Sum();
+        }
+
+        private static void ThrowIfAnyNegative(List<int> numbersList)
+        {
             if (numbersList.Any(x => x < 0))
             {
                 string negativeNumbers = string.Join(",", numbersList.Where(x => x < 0));
                 string errorMessage = string.Format("Negative numbers not allowed[{0}]", negativeNumbers);
                 throw new ArgumentException(errorMessage, "numbers");
             }
-
-            return numbersList.Sum();
         }
     }
 }
