7cba4f3005d7854fe57ddf5d52a38a5f52b90c6d
Sriram <sriram06sept@gmail.com>
2015-02-01 22:35:47 +0400

    Custom delimiter support added

---
+++ StringCalculatorKata.UnitTest/StringCalculator_AddShould.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorKata.UnitTest
+{
+    [TestFixture]
+    public class StringCalculator_AddShould
+    {
+        [Test]
+        public void ReturnZero_GivenEmptyString()
+        {
+            StringCalculator sut = new StringCalculator();
+
+            int result = sut.Add("");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase("1", 1), TestCase("1,2", 3),
+        TestCase("1,2,3,4", 10)]
+        public void AddNumbers_GivenCommaSeparatedStrings(string numbers, int expected)
+        {
+            StringCalculator sut = new StringCalculator();
+
+            int result = sut.Add(numbers);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("1\n2,3", 6)]
+        public void AddNumbers_GivenNewLineSeparatedStrings(string numbers, int expected)
+        {
+            StringCalculator sut = new StringCalculator();
+
+            int result = sut.Add(numbers);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("//;\n1;2", 3)]
+        public void AddNumbers_GivenCustomDelimiterSeparatedStrings(string numbers, int expected)
+        {
+            StringCalculator sut = new StringCalculator();
+
+            int result = sut.Add(numbers);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,13 +1,34 @@
+using System;
 using System.Linq;
 
 namespace StringCalculatorKata
 {
+    //"//;\n1;2"
     public class StringCalculator
     {
         public int Add(string numbers)
         {
-            return numbers
-                .Split(',', '\n')
+            const string newLine = "\n";
+            const string customDelimiterStart = "//";
+
+            string input;
+            string[] delimiters;
+            if (numbers.StartsWith(customDelimiterStart) && numbers.Contains(newLine))
+            {
+                int end = numbers.IndexOf(newLine);
+                string customDelimiter = numbers.Substring(2, end - 2);
+
+                delimiters = new[] { customDelimiter, newLine };
+                input = numbers.Substring(end + newLine.Length);
+            }
+            else
+            {
+                input = numbers;
+                delimiters = new[] { ",", newLine };
+            }
+
+            return input
+                .Split(delimiters, StringSplitOptions.None)
                 .Sum(item => item.TryParseOrDefault());
         }
     }
